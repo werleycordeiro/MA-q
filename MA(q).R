110ac19f89d4e0f52bae51c4cba4d47f31d91168
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-27 17:41:27 -0300

    Update MA(q).R

--- MA(q).R
+++ MA(q).R
@@ -15,26 +15,9 @@ q = 2
 para = matrix(0.2,1,(2+q))
 
 para[1] = mean(data)
-para[(2+q)] = var(data) 
+para[(2+q)] = var(data)
 
-ma_q = function(para,data,q){
-
-mu = para[1]
-theta = para[2:(q+1)]
-sigma2 = para[(2+q)]
-e = matrix(0,n,1)
-n = dim(data)[1]
-
-loglik = -.5 * n * log(2 * pi)
-
-for(i in 1:dim(data)[1]){
-	e[i+q] = data[i]-mu-(theta %*% e[(1+i-1):(q+i-1)])
-	e2 = (e[i+q])^2 
-	loglik = loglik - .5 * log(sigma2)-(e2/(2*sigma2))
-	}
-return(-loglik)
-}
-
+source("ma_q.R")
 ma_q(para=para,data=data,q=q)
 
 low = c(rep(-Inf,q+1),0)
